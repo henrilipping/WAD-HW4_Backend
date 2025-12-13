19cad9605aff46e730c9dcc2a1e3b4909fd81c64
Henri <henriest12@gmail.com>
2025-12-13 12:10:14 +0200

    Creates SQL table, generates JWT, checks user authinticated, signup a user system, login a user system, logout a user system

--- database.js
+++ database.js
@@ -8,4 +8,32 @@ const pool = new Pool({
     port: "5432"
 });
 
+// Executes a SQL query
+const execute = async(query) => {
+    try {
+        await pool.connect();
+        await pool.query(query);
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    }
+};
+
+// Creates table in SQL
+const createTblQuery = `
+    CREATE TABLE IF NOT EXISTS "users" (
+        id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+        email VARCHAR(200) NOT NULL UNIQUE,
+        password VARCHAR(200) NOT NULL 
+    );`;
+
+// Executes the createTblQuery on application startup
+execute(createTblQuery).then(result => {
+    if (result) {
+        console.log('Table "users" is created');
+    }
+});
+
+// Exports the database connection pool to other files
 module.exports = pool;
--- server.js
+++ server.js
@@ -11,16 +11,100 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
-// We need to include "credentials: true" to allow cookies to be represented
-// Also "credentials: 'include'" need to be added in Fetch API in the Vue.js App
 
 app.use(express.json());  // Parses incoming requests with JSON payloads and is based on body-parser.
 app.use(cookieParser());  // Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
 
-``````````````
-    // Code will go here
+const secret = "3sja39shf93sh3ksha1923030f02jabdakdazmeri372bw2sh2dw";
+const maxAge = 60 * 60; // seconds
 
-    ``````````````
+// Generates to user a JWT
+const generateJWT = (id) => {
+    return jwt.sign({ id }, secret, { expiresIn: maxAge })
+}
+
 app.listen(port, () => {
     console.log("Server is listening to port " + port)
+});
+
+
+// To check whether a user is authinticated
+app.get('/auth/authenticate', async(req, res) => {
+    console.log('authentication request has been arrived');
+    const token = req.cookies.jwt;
+    let authenticated = false;
+    try {
+        if (token) { // Checks if the token exists
+            await jwt.verify(token, secret, (err) => {
+                if (err) { // not verified
+                    console.log(err.message);
+                    console.log('token is not verified');
+                    res.send({ "authenticated": authenticated }); // authenticated = false
+                } else { // token exists and it is verified
+                    console.log('author is authinticated');
+                    authenticated = true;
+                    res.send({ "authenticated": authenticated }); // authenticated = true
+                }
+            })
+        } else {
+            console.log('author is not authinticated');
+            res.send({ "authenticated": authenticated }); // authenticated = false
+        }
+    } catch (err) {
+        console.error(err.message);
+        res.status(400).send(err.message);
+    }
+});
+
+// Signup a user
+app.post('/auth/signup', async(req, res) => {
+    try {
+        console.log("a signup request has arrived");
+        const { email, password } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const bcryptPassword = await bcrypt.hash(password, salt)
+        const authUser = await pool.query(
+            "INSERT INTO users(email, password) values ($1, $2) RETURNING*", [email, bcryptPassword]
+        );
+        console.log(authUser.rows[0].id);
+        const token = await generateJWT(authUser.rows[0].id);
+        res
+            .status(201)
+            .cookie('jwt', token, { maxAge: 6000000, httpOnly: true })
+            .json({ user_id: authUser.rows[0].id })
+            .send;
+    } catch (err) {
+        console.error(err.message);
+        res.status(400).send(err.message);
+    }
+});
+
+// Login a user
+app.post('/auth/login', async(req, res) => {
+    try {
+        console.log("a login request has arrived");
+        const { email, password } = req.body;
+        const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+        if (user.rows.length === 0) return res.status(401).json({ error: "User is not registered" });
+
+        // Checking if the password is correct
+        const validPassword = await bcrypt.compare(password, user.rows[0].password);
+        if (!validPassword) return res.status(401).json({ error: "Incorrect password" });
+
+        const token = await generateJWT(user.rows[0].id);
+        res
+            .status(201)
+            .cookie('jwt', token, { maxAge: 6000000, httpOnly: true })
+            .json({ user_id: user.rows[0].id })
+            .send;
+    } catch (error) {
+        res.status(401).json({ error: error.message });
+    }
+});
+
+// Logout a user and deletes the jwt
+app.get('/auth/logout', (req, res) => {
+    console.log('delete jwt request arrived');
+    res.status(202).clearCookie('jwt').json({ "Msg": "cookie cleared" }).send
 });
